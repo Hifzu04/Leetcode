44352b8bf976a2e94ab7c8c5eb0188b491710e06
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2025-10-26 19:19:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.33%) - LeetHub

---
+++ 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long removeZeros(long long n) {
+        string s = to_string(n);
+        string p ="";
+        for(int i = 0 ; i< s.size() ; i++){
+            if(s[i] != '0') p += s[i];
+        }
+       return  stoll(p);
+      
+    }
+};
