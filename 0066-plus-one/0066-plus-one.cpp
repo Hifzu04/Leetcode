c87bf57b95e1fe627f7df6f9165468a6be6017a1
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-01-01 23:04:18 +0530

    Time: 0 ms (100%), Space: 11.4 MB (77.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digit) {
+     int  n =  digit.size();
+     for(int  i = n-1 ; i>=0 ; i--){
+        if(digit[i]  != 9){
+            digit[i] +=1 ;
+            return digit;
+        }
+        digit[i] = 0 ;
+     }
+     digit.insert(digit.begin() , 1);
+     return digit;
+     
+     
+     }
+
+    
+
+   
+    
+};
