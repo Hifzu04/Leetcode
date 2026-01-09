55048494e62cb16b7da91a402299052009e88fe0
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-01-09 06:39:08 +0530

    Time: 0 ms (100%), Space: 15 MB (63.11%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -16,7 +16,7 @@ class Solution {
 public:
 
     //using level order traversal (BFS)
-    void f(TreeNode* root , vector<int>&ans){
+    void bfs(TreeNode* root , vector<int>&ans){
           if(!root) return  ;
        
         queue<TreeNode*>q;
@@ -37,9 +37,24 @@ public:
 
         }
     }
+
+
+    //using dfs - inorder
+    void dfs(TreeNode* root , vector<int>&ans ,int  level ){
+              if(!root) return ;
+              if(ans.size() < level){
+                 ans.push_back(root->val);
+              } 
+              if(root->right) dfs(root->right , ans , level+1);
+            if(root->left)    dfs(root->left , ans , level+1 );
+
+    }
+   
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans; 
-        f(root , ans);
+       // bfs(root , ans);
+
+        dfs(root , ans ,1 );
         return ans ;
 
     }
