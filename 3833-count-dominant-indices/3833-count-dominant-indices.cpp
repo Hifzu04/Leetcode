bba862d3ff99fd8b4919fe8b09aa673160a658d1
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-02-08 17:08:16 +0530

    Time: 0 ms (100%), Space: 33.2 MB (11.93%) - LeetHub

---
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dominantIndices(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<float>avg(n);
+        int sum = 0 ;
+        int cnt = 1;
+        for(int i = n-1 ; i>= 0 ; i--){
+            sum += nums[i];
+            avg[i] = sum/cnt; 
+            cnt++;
+        }
+        int ans = 0;
+        for(int i = 0 ; i< n-1; i++){
+             if(nums[i] > avg[i+1]){
+                ans++;
+             }
+        }
+        return ans;
+    }
+};
