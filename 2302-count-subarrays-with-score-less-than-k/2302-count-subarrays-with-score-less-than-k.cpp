41925f30b994fd1f39b556de5cbbe0214995ea3e
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-02-09 00:15:16 +0530

    Time: 7 ms (20.57%), Space: 99 MB (41.51%) - LeetHub

---
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int i = 0 ; 
+        int j = 0;
+        long long sum = 0;
+        long long score =  0 ; 
+        while(j<nums.size()){
+            sum+=nums[j];
+            while(i<=j &&  (sum * (j-i+1) >= k)){
+                   sum -= nums[i];
+                    i++;
+            }
+
+            score +=  (j-i+1);
+            j++;
+        }
+        return score ;
+        
+    }
+};
