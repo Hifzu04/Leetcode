39635ae9fe3ab404b863c541a5fa87e90f9195fe
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-02-18 17:36:56 +0530

    Time: 99 ms (22.15%), Space: 135.9 MB (60.8%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+  typedef pair<int, pair<int , int>>  P;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2 ,  int k) {
+        //maxHeap
+        priority_queue<P, vector<P>>pq;
+
+        for(int i = 0 ; i< nums1.size() ;i++){
+            for(int j = 0 ; j< nums2.size() ; j++){
+                int sum = nums1[i] + nums2[j];
+
+                if(pq.size() < k){
+                    pq.push({sum , {i,j} });
+                }else if (pq.top().first > sum){
+                    pq.pop();
+                    pq.push({sum , {i,j}});
+                }else {
+                    break;
+                }
+            
+            }
+        }
+      vector<vector<int>>ans;
+
+        while(!pq.empty()){
+            auto temp = pq.top();
+            pq.pop();
+
+            int i = temp.second.first;
+            int j = temp.second.second;
+            ans.push_back({nums1[i] , nums2[j]});
+            
+
+        }
+
+        
+        return ans;
+    }
+};
