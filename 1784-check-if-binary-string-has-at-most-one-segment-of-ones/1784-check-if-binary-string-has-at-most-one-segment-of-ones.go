ad1f394f97e29b33f0560d57cb0d22f1d7c6eab1
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-03-07 00:36:31 +0530

    Time: 0 ms (100%), Space: 3.8 MB (91.67%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.go
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.go
@@ -1,8 +1,3 @@
 func checkOnesSegment(s string) bool {
-    for i := 1 ; i< len(s) ; i++ {
-        if s[i-1] == '0' && s[i] == '1' {
-            return false ;
-        }
-    }
-    return true;
+  return !strings.Contains(s , "01");
 }
