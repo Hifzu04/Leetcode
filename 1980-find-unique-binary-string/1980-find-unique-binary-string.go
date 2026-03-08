fb142de50d9b52a2db9fdc3f25a2369e82cd2ff4
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-03-08 16:47:39 +0530

    Time: 0 ms (100%), Space: 4.1 MB (80%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.go
@@ -0,0 +1,11 @@
+func findDifferentBinaryString(nums []string) string {
+	ans := ""
+	for i := 0; i < len(nums); i++ {
+		if nums[i][i] == '0' {
+			ans += "1"
+		} else {
+			ans += "0"
+		}
+	}
+	return ans
+}
