4b97b39b2d07850c74c1604ad90d4917cbf209af
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-06-02 01:19:51 +0530

    Time: 4 ms (51.29%), Space: 14.7 MB (54.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int>ans;
+//         for(int  i = 0 ; i< nums.size()  ; i++){
+//             for(int j = i+1 ; j< nums.size() ; j++) {
+//                 if(i != j && nums[i] == target -nums[j]) {
+//                    ans.push_back(i);
+//                    ans.push_back(j);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>ans;
-        for(int  i = 0 ; i< nums.size()  ; i++){
-            for(int j = i+1 ; j< nums.size() ; j++) {
-                if(i != j && nums[i] == target -nums[j]) {
-                   ans.push_back(i);
-                   ans.push_back(j);
-                }
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+
+            if (mp.count(need)) {
+                return {mp[need], i};
             }
+
+            mp[nums[i]] = i;
         }
-        return ans;
-    } 
+
+        return {};
+    }
 };
