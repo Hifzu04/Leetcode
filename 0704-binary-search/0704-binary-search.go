feca5166a3b1763469682b0f7bce0e99bf0784e1
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-07-04 01:30:38 +0530

    Time: 0 ms (100%), Space: 8.6 MB (75.77%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.go
@@ -0,0 +1,16 @@
+func search(arr []int, target int) int {
+     l :=0  
+      r := len(arr)-1;
+
+        for l<= r {
+             mid := l + (r-l) /2  ;
+            if arr[mid] == target{
+                return mid;
+            }else if arr[mid] < target{
+                l= mid+1 ;
+            }else {
+                r = mid-1;
+            }
+        }
+        return -1 ;
+}
