bc46526ec514febae5e94d1d50d871d7584ef31a
Hifzur  Rahman <148140700+Hifzu04@users.noreply.github.com>
2026-08-01 23:37:07 +0530

    Time: 1476 ms (6.61%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+
+select r.contest_id  , ifnull(round((count(u.user_id) * 100)/(select count(*) from Users), 2), 0) as percentage
+from Users u 
+ join Register r on 
+u.user_id = r.user_id 
+group by r.contest_id
+order by percentage desc , contest_id ;
